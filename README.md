6e0fe94c6910a7cb92b61b5750e719cf6929107c
phquang98 <phquang98@gmail.com>
2022-01-23 01:38:45 +0200

    feat: ini envi

---
+++ README.md
@@ -0,0 +1,11 @@
+# Template Backend Pure
+
+Last updated: 30/12/2021
+Tech: TS + Express
+
+## Run
+
+- `npm i`
+- `npm run build`: let `tsc` compile Node server in TS to JS, and compile again detect changes in `/src`
+- `npm start`: build a Node server using `nodemon`
+- remember install `.d.ts` files for any libraries with `npm i -D @types/yourLibHere`
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import express from "express";
+
+// --- Config + Initiate server ---
+dotenv.config(); // read key-value pairs from .env
+const port = process.env.PORT_NUMBER_HERE || 3000; //
+
+const app = express(); // create an express app server
+
+// --- Top Lv Middlewares ---
+app.use(express.json()); // TLDR can send json data from FE to endpoints
+app.use(express.urlencoded({ extended: true })); // if use Form submit, data from form will be written to req.body
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms")); // TLDR logger
+app.use(cors()); // TLDR allow dif origin to HTTP to this server
+
+// --- Run server ---
+app.get("/healthcheck", (req, res, _next) => {
+  return res.status(200).json({ msg: "Hello there!" });
+});
+
+app.post("/healthcheck", (req, res, _next) => {
+  if (req.body.myNameIs) {
+    return res.status(200).json({ msg: "Request received with desired key", reqBody: req.body });
+  } else {
+    return res.status(400).json({ msg: "Bad request! Maybe req.body is missing key prop!" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
+
+// checking
